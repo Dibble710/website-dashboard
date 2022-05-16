a9e6f82c003f68e19680aacff9d5f99b8e990016
Tyler Antoni <tylerantoni1025@icloud.com>
2022-05-15 22:02:20 -0400

    New footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,17 @@ function Footer() {
   return (
     <>
       <div className="container">
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <p className="col-md-4 mb-0 text-muted">© 2022 Digitizers, Inc</p>
-          <ul className="nav col-md-4 justify-content-end">
+        <footer className="py-3 my-4 justify-content-center">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
             <li className="nav-item">
               <Link to="/" className="nav-link px-2 text-muted">
                 Home
               </Link>
             </li>
           </ul>
+            <Link to="/">
+              <p className="text-center text-muted">© 2022 Digitizers, Inc</p>
+            </Link>
         </footer>
       </div>
     </>
